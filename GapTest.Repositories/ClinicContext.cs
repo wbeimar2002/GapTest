0d92cc91bc2f308ff5284a77507c723aaefb91c2
Alexander Gonzalez <wbeimar2002@hotmail.com>
2020-01-11 13:15:48 -0500

    importing nuget packages and creating dbContext

---
+++ GapTest.Repositories/ClinicContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace GapTest.Repositories
+{
+    public class ClinicContext: DbContext
+    {
+
+    }
+}
