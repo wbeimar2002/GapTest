ceeb6330694d15965154cd07b33bd0e7c53d62c5
Alexander Gonzalez <wbeimar2002@hotmail.com>
2020-01-12 11:41:02 -0500

    Add the initial migrations

--- GapTest.Repositories/ClinicContext.cs
+++ GapTest.Repositories/ClinicContext.cs
@@ -1,8 +1,5 @@
 ﻿namespace GapTest.Repositories
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
     using Models.Entities;
     using Microsoft.EntityFrameworkCore;
 
---
+++ GapTest.Repositories/Migrations/20200112163648_InitialCreate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GapTest.Repositories.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    IdentificationNumber = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialty", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    PatientId = table.Column<int>(nullable: true),
+                    SpecialtyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointment_Patient_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Appointment_Specialty_SpecialtyId",
+                        column: x => x.SpecialtyId,
+                        principalTable: "Specialty",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Specialty",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "General Medicine" });
+
+            migrationBuilder.InsertData(
+                table: "Specialty",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Dentistry" });
+
+            migrationBuilder.InsertData(
+                table: "Specialty",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Pediatrics" });
+
+            migrationBuilder.InsertData(
+                table: "Specialty",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "Neurology" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointment_PatientId",
+                table: "Appointment",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointment_SpecialtyId",
+                table: "Appointment",
+                column: "SpecialtyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment");
+
+            migrationBuilder.DropTable(
+                name: "Patient");
+
+            migrationBuilder.DropTable(
+                name: "Specialty");
+        }
+    }
+}
